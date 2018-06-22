4bbb0c323a3fe791809a0a0f4e5f84da1db0d689
Grant Atkins <gatki001@odu.edu>
2018-06-22 10:21:27 -0600

    Update R vis scripts for multi-month.

--- memento-heatmap.R
+++ memento-heatmap.R
@@ -3,12 +3,16 @@ library(ggplot2)
 format_data <- function(dt){
   # format uri removing www
   dt$uri <- gsub("(www.)?", "", dt$uri)
-  # swap from GMT to EST time
+  dt <- dt[!(dt$uri == "wsj.com"),]
+  # freq <- ave(rep(1, times=nrow(dt)), dt$uri, FUN=sum)
+  # dt[order(freq, dt$uri, decreasing = TRUE), ]
+    # swap from GMT to EST time
   dt$datetime <- as.POSIXct(dt$datetime, format='%Y-%m-%dT%H:%M:%S', tz="GMT")
   dt$datetime <- format(dt, tz="America/New_York")$datetime
   dt$datetime <- strptime(dt$datetime, format = '%Y-%m-%d %H:%M:%S')
   # Getting the day and hour of each memento pushed
   dt$weekday <- weekdays(as.Date(dt$datetime))
+  dt$month <- months(as.Date(dt$datetime))
   dt$hour <- dt$datetime$hour
   dt
 }
@@ -24,21 +28,41 @@ avg_times <- function(dailyMementos){
     temp[nrow(temp) + 1,] = list(i, floor(avg))
   }
   print(temp[which.max(temp$avg),])
+  print(temp[which.min(temp$avg),])
   # print(max(temp$V2))
 }
+
+# count_mementos_per_uri <- function(dt){
+#   # find average for each day
+#   temp <- data.frame(hour=double(), avg=double())
+#   for(i in unique(dt$)){
+#     s <- subset(dailyMementos, Hour == i)
+#     m <- sum(s$Freq)
+#     avg <- m / length(unique(dailyMementos$uri))
+#     # print(paste("Hour",i, "avg =",floor(avg)))
+#     temp[nrow(temp) + 1,] = list(i, floor(avg))
+#   }
+#   print(temp[which.max(temp$avg),])
+#   # print(max(temp$V2))
+# }
 
 make_plots <- function(dt, plot_title){
   dt <- as.data.frame(dt)
   dailyMementos <- as.data.frame(table(dt$uri, dt$hour))
   names(dailyMementos) <- c('uri', 'Hour', 'Freq')
   dailyMementos$Hour <- as.numeric(as.character(dailyMementos$Hour))
-  dailyMementos$uri <- factor(dailyMementos$uri, ordered = TRUE, 
+  dailyMementos$uri <- factor(dailyMementos$uri,  
                               levels = unique(dt$uri))
   # Plotting the number of crimes each day (line graph)
-  # linechart <- ggplot(dailyMementos, aes(x = Hour, y = Freq)) + geom_line(aes(group = uri, color = uri)) + xlab('Hour') + ylab('Number of Mementos') + ggtitle('Hourly count of Mementos created - November 2016')
+  # linechart <- ggplot(dailyMementos, aes(x = Hour, y = Freq)) + geom_line(aes(group = uri, color = uri)) + xlab('Hour') + ylab('Number of Mementos') + theme(plot.title = element_text(hjust = 0.5)) + ggtitle(plot_title)
   # print(linechart)
   # auto generate graph
-  hmap <- ggplot(dailyMementos, aes(x = Hour, y = uri)) + geom_tile(aes(fill = Freq)) + scale_fill_gradient(name = 'Mementos created', low = 'white', high = 'red') + theme(axis.title.y = element_blank(), plot.title = element_text(hjust = 0.5)) + ggtitle(plot_title)
+  hmap <- ggplot(dailyMementos, aes(x = Hour, y = uri)) + geom_tile(aes(fill = Freq)) +
+    scale_fill_gradient(name = 'Mementos created', low = 'white', high = 'red') + 
+    theme(axis.title.y = element_blank(), plot.title = element_text(hjust = 0.5)) + 
+    # scale_x_discrete(labels = c("0" = "12AM", "5" = "5AM", "10" = "10AM", "15" = "3PM", "20" = "8PM")) +
+    scale_x_continuous(breaks=c(0,5,10,15,20), labels=c("12AM", "5AM", "10AM", "3PM", "8PM")) +
+    ggtitle(plot_title)
   print(hmap)
 
   dailyMementos
@@ -46,15 +70,29 @@ make_plots <- function(dt, plot_title){
 
 path = "./data/mementos-per-month/"
 file.names <- dir(path, pattern =".csv")
-pdf(paste("./docs/imgs/heatmaps/mementos-per-hour", ".pdf",sep = ""))
+# pdf(paste("./docs/imgs/mementos/mementos-per-hour-line-graph", ".pdf",sep = ""))
 for(i in 1:length(file.names)){
   plot_title <- gsub('.{4}$', '', file.names[i])
+  cat(paste(plot_title, "\n"))
   dt <- read.csv(paste(path, file.names[i], sep = ""), header = TRUE)
   print(plot_title)
   # data tables allow in place modification
   dt <- format_data(dt)
   dailyMementos <- make_plots(dt, plot_title)
   avg_times(dailyMementos)
-  # break
+  break
 }
-dev.off()
+# dev.off()
+
+# 
+# Stats per months
+# 
+temp <- read.csv("./data/mementos-per-month/3months.csv")
+temp <- format_data(temp)
+temp <- subset(temp, month == "November" | month == "December" | month == "January")
+dailyMementos <- make_plots(temp, "")
+avg_times(dailyMementos)
+# 
+# for(i in unique(temp$uri)){
+#   cat(paste("URI",i,"&",sum(temp$uri == i),"\n"))
+# }
--- memento_time_diff.R
+++ memento_time_diff.R
@@ -1,47 +1,73 @@
+library(tidyverse)
 library(ggplot2)
 
-dt <- read.csv("data/headline-counts.csv", header = TRUE)
+dt <- read.csv("data/parsed_links/headline-counts.csv", header = TRUE)
 # remove wsj
 dt <- dt[dt$uri != "www.wsj.com", ]
+# format uri removing www
+dt$uri <- gsub("(www.)?", "", dt$uri)
 
 dailyMementos <- as.data.frame(dt)
 dailyMementos$headline_count <- as.numeric(dailyMementos$headline_count)
-dailyMementos$day <- as.numeric(as.character(dailyMementos$day))
+dailyMementos$day <- as.POSIXct(dailyMementos$day, format='%Y-%m-%d', tz="GMT")
 dailyMementos$uri <- factor(dailyMementos$uri, ordered = TRUE, 
                             levels = unique(dt$uri))
+dailyMementos <- subset(dailyMementos, months(day) == "November" | months(day) == "December" | months(day) == "January")
+## Add a column indicating what days to highlight
+# dailyMementos <- dailyMementos %>% mutate( ToHighlight = ifelse( months(day) == "November", "yes", "no" ) )
 # Plotting the number of headlines each day (line graph)
-p <- ggplot(dailyMementos, aes(x = day, y = headline_count)) + 
+p <- ggplot(dailyMementos, aes(x = as.Date(day), y = headline_count)) + 
   # geom_point(aes(group = uri, color = uri)) + 
   geom_bar(stat = "identity") +
   xlab('Day') + ylab('Number of stories') + 
-  ggtitle('Total count of stories found - 1AM GMT - November 2016 ') +
+  # ggtitle('Total count of stories found') +
+  # scale_fill_manual( values = c( "yes"="black", "no"="black" ), guide = FALSE ) +
+  scale_y_continuous(breaks=c(0,5,10)) +
   theme(plot.title = element_text(hjust = 0.5))
-# p <- p + facet_wrap(~uri, ncol = 2)
+p <- p + facet_wrap(~uri, ncol = 2)
 # auto generate graph
 print(p)
 
-times <- read.csv("data/memento-times.csv", header = TRUE)
+# Add weekdays to data frame
+# for(i in 1:nrow(dailyMementos)){
+#   date <- paste("2016", "11", sprintf("%02d", dailyMementos[i, "day"]), sep="-")
+#   date <- as.POSIXct(date, format='%Y-%m-%d', tz="GMT")
+#   dailyMementos[i, "weekday"] <- weekdays(as.Date(date))
+# }
+
+times <- read.csv("data/mementos/memento-times.csv", header = TRUE)
 # remove wsj
 times <- times[times$uri != "www.wsj.com", ]
+# format uri removing www
+times$uri <- gsub("(www.)?", "", times$uri)
 times$actual_datetime <- as.POSIXct(times$actual_datetime, format='%a, %d %b %Y %H:%M:%S GMT', tz="GMT")
 times$request_datetime <- as.POSIXct(times$request_datetime, format='%Y-%m-%dT%H:%M:%S', tz="GMT")
+times <- subset(times, months(request_datetime) == "November" | months(request_datetime) == "December" | months(request_datetime) == "January")
 
 # time difference in minutes to one significant figure
 times$diff <- difftime(times$actual_datetime, times$request_datetime, units = "min")
 # convert day to factor
 dailyTimes <- as.data.frame(times)
-dailyTimes$day <- as.numeric(as.character(dailyTimes$day))
+# dailyTimes$day <- as.numeric(as.character(dailyTimes$day))
 dailyTimes$diff <- as.numeric(dailyTimes$diff)
 dailyTimes$uri <- factor(dailyTimes$uri, ordered = TRUE, 
                             levels = unique(times$uri))
 
-# Plotting the number of headlines each day (line graph)
-time_plot <- ggplot(dailyTimes, aes(x = day, y = diff)) + 
+# Plotting the number of headlines each day (bar graph)
+time_plot <- ggplot(dailyTimes, aes(x = actual_datetime, y = diff)) + 
   geom_point() + 
-  scale_y_continuous(trans='log10') +
+  # scale_y_continuous(trans='log10') +
   xlab('Day') + ylab('Time Difference (Minutes)') + 
-  ggtitle('Request vs. Actual timestamps - 1AM GMT - November 2016 ') + 
-  geom_text(aes(label=ifelse(diff>100 | diff < -100,as.character(uri),'')),hjust=-0.05,vjust=0,) 
+  ggtitle('Memento Request vs. Actual timestamps - 1AM GMT') +
+  geom_text(aes(label=ifelse(diff>250 | diff < -250,as.character(uri),'')),hjust=-0.05,vjust=0) +
+  theme(plot.title = element_text(hjust = 0.5))
   # scale_x_continuous(breaks = round(seq(min(dailyTimes$day), max(dailyTimes$day), by = 1),1)) 
 # auto generate graph
 print(time_plot)
+
+# Boxplot for time differences in memento request time and actual memento time
+bplot <- ggplot(dailyTimes, aes(x = uri, y = diff)) + 
+  geom_boxplot() + 
+  xlab('URI') + ylab('Time Difference (Minutes)') + 
+  theme(axis.text.x = element_text(angle = 60, hjust = 1))
+print(bplot)
